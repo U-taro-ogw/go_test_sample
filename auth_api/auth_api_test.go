3ccd4d26a842d257a7f6a8b3ae938b0e17f1f452
U-taro-ogw <yuutarou0406@gmail.com>
2020-02-19 01:00:01 +0900

    [WIP]signin 実装

--- auth_api/auth_api_test.go
+++ auth_api/auth_api_test.go
@@ -32,7 +32,6 @@ var _ = Describe("AuthApi", func() {
 	// /v1/signupへのrequest spec
 	Describe("Signup", func() {
 		Context("POSTパラメータが存在する場合", func() {
-
 			Context("email password が blank でない場合", func() {
 				BeforeEach(func() {
 					postParameter.Email = "foo@example.com"
@@ -96,6 +95,49 @@ var _ = Describe("AuthApi", func() {
 				r.ServeHTTP(w, req)
 
 				Expect(w.Code).To(Equal(400))
+			})
+		})
+	})
+
+
+	// /v1/signinへのrequest spec
+	Describe("Signin", func() {
+		Context("POSTパラメータが存在する場合", func() {
+			Context("パラメータ通りのuserが存在する場合", func() {
+				BeforeEach(func() {
+					postParameter.Email = "foo@example.com"
+					postParameter.Password = "password"
+				})
+
+				It("200を返却する", func() {
+					sampleJson, _ := json.Marshal(postParameter)
+					body := bytes.NewBuffer(sampleJson)
+
+					req, _ := http.NewRequest("POST", "v1/signin", body)
+					r.ServeHTTP(w, req)
+					Expect(w.Code).To(Equal(200))
+				})
+
+				It("jwt tokenを返却する", func() {
+
+				})
+
+				It("jwt tokenを保存する", func() {
+
+				})
+			})
+
+			Context("パラメータ通りのuserが存在しない場合", func() {
+				It("401エラーを返却する", func() {
+
+				})
+			})
+
+		})
+
+		Context("POSTパラメータが存在しない場合", func() {
+			It("401エラーを返却する", func() {
+
 			})
 		})
 	})
