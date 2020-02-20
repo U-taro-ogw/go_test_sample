7f6c9133a11d3b88f9570ce07b7baaf23e01a72a
U-taro-ogw <yuutarou0406@gmail.com>
2020-02-21 01:35:23 +0900

    [WIP]signin テスト
    - response bodyの検証

--- auth_api/modules/jwt_token.go
+++ auth_api/modules/jwt_token.go
@@ -1,9 +1,11 @@
 package modules
 
 import (
-jwt "github.com/dgrijalva/jwt-go"
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
+// 関数は1つ以上の引数を取るべきだ
+// って論があったはず
 func GenerateJwtToken() string {
 	// headerのセット
 	token := jwt.New(jwt.SigningMethodHS256)
