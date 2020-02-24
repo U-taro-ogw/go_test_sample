0e9fc1d02e41c1bac979354bf386a1a665306551
U-taro-ogw <yuutarou0406@gmail.com>
2020-02-24 23:44:58 +0900

    api準備

--- flask_api_one/app.py
+++ flask_api_one/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from redis import Redis
+from time import sleep
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -18,20 +19,30 @@ def redis_hits():
 
 
 @app.route('/api_info', methods=['GET'])
-def search():
+def api_info():
     jwt_token = request.headers.get("Authorization")
     auth = redis.get(jwt_token)
     if not auth:
         return jsonify({"error": "Unauthorized"}), 401
 
-    ret_dict = {
+    api_response = {
         "api_name": "flask_api_one",
         "info": {
             "language": "python",
             "framework": "flask"
         }
     }
-    return jsonify(ret_dict), 200
+    return jsonify(api_response), 200
+
+@app.route('/sleep_api', methods=['GET'])
+def sleep_api():
+    sleep(5)
+    api_response = {
+        "api_name": "flask_api_one",
+        "sleep_time": 5
+    }
+    return jsonify(api_response), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
