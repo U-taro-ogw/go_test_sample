fd6cd8a8018873f2f6ca5aabc81fb08278949e42
U-taro-ogw <51327986+U-taro-ogw@users.noreply.github.com>
2020-03-10 15:57:27 +0900

    テストコード実装中

--- bff_api/bff_api_test.go
+++ bff_api/bff_api_test.go
@@ -10,31 +10,25 @@ import (
 	. "github.com/U-taro-ogw/go_test_sample/bff_api"
 
 	"fmt"
-	"reflect"
+	//"reflect"
 	"net/http"
 	"net/http/httptest"
+	"encoding/json"
 )
 
+var app App
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	app.Router.ServeHTTP(rr, req)
+	return rr
+}
+
 var _ = Describe("BffApi", func() {
-	//apiHandler := ApiHandler{
-	//	ApiOneUrl: "http://flask_api_one:5000",
-	//	ApiTwoUrl: "http://flask_api_one:6000",
-	//}
-	//r := GetMainEngine(apiHandler)
-	w := httptest.NewRecorder()
-
-	//var server *ghttp.Server
-	//var statusCode int
-
 	BeforeEach(func() {
-		w = httptest.NewRecorder()
-		//server = ghttp.NewServer()
-		//server.AppendHandlers(ghttp.CombineHandlers(ghttp.VerifyRequest("GET", "http://flask_api_one:5000/api_info")))
+		app = App{}
+		app.Initialize()
 	})
-
-	//AfterEach(func() {
-	//	server.Close()
-	//})
 
 	Describe("Apis", func() {
 		Context("Request Headerにjwt tokenが存在する場合", func() {
@@ -42,60 +36,61 @@ var _ = Describe("BffApi", func() {
 				// 他APIへRequestできる状態
 				Context("全てのAPIから200が返却された場合", func() {
 					It("200を返却する", func() {
-						hoge := GetApisInfo()
-						fmt.Println(hoge)
-						fmt.Println(reflect.TypeOf(hoge))
+						req, _ := http.NewRequest("GET", "/v1/apis", nil)
+						response := executeRequest(req)
 
+						fmt.Println("response.Code")
+						fmt.Println(response.Code)
+						fmt.Println("response.Body.String()")
+						fmt.Println(response.Body.String())
 
-						//req, _ := http.NewRequest("GET", "v1/apis", nil)
-						//r.ServeHTTP(w, req)
-						//fmt.Println(w)
-						Expect(1).To(Equal(204))
+						res := map[string]string{"key":"value"}
+						bytes, _ := json.Marshal(res)
+
+						Expect(response.Body.String()).To(Equal(string(bytes)))
 					})
 
-					It("API情報を返却する", func() {
-
-					})
+					//It("API情報を返却する", func() {})
 				})
 
 				Context("一部のAPIから200以外が返却された場合", func() {
 					Context("401の場合", func() {
-						It("401を返却する", func() {})
+						//It("401を返却する", func() {})
 					})
 
 					Context("403の場合", func() {
-						It("403を返却する", func() {})
+						//It("403を返却する", func() {})
 					})
 
 					Context("500の場合", func() {
-						It("500を返却する", func() {})
+						//It("500を返却する", func() {})
 					})
 				})
 
 				Context("全てのAPIから200以外が返却された場合", func() {
 					Context("401の場合", func() {
-						It("401を返却する", func() {})
+						//It("401を返却する", func() {})
 					})
 
 					Context("403の場合", func() {
-						It("403を返却する", func() {})
+						//It("403を返却する", func() {})
 					})
 
 					Context("500の場合", func() {
-						It("500を返却する", func() {})
+						//It("500を返却する", func() {})
 					})
 				})
 			})
 
 			Context("Redisに認証情報が存在しない場合", func() {
 				// 401を返却する
-				It("401を返却する", func() {})
+				//It("401を返却する", func() {})
 			})
 		})
 
 		Context("Request Headerにjwt tokenが存在しない場合", func() {
 			// 401を返却する
-			It("401を返却する", func() {})
+			//It("401を返却する", func() {})
 		})
 	})
 })
