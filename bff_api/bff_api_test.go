5028d92649702ffef1c3dce5ca461c32b5cd218a
U-taro-ogw <51327986+U-taro-ogw@users.noreply.github.com>
2020-03-13 14:31:16 +0900

    修正

--- bff_api/bff_api_test.go
+++ bff_api/bff_api_test.go
@@ -1,19 +1,18 @@
 package main_test
 
 import (
-	//. "github.com/onsi/ginkgo/tmp"
-	//"github.com/onsi/gomega/ghttp"
-
+	. "github.com/U-taro-ogw/go_test_sample/bff_api"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	. "github.com/U-taro-ogw/go_test_sample/bff_api"
+	//"github.com/jarcoal/httpmock"
+	//"github.com/onsi/gomega/ghttp"
 
+	"encoding/json"
 	"fmt"
-	//"reflect"
 	"net/http"
 	"net/http/httptest"
-	"encoding/json"
+	//"os"
 )
 
 var app App
@@ -25,17 +24,53 @@ func executeRequest(req *http.Request) *httptest.ResponseRecorder {
 }
 
 var _ = Describe("BffApi", func() {
+
+	//var server *ghttp.Server
+	//var returnedJson string
+	//var statusCode int
+
 	BeforeEach(func() {
 		app = App{}
 		app.Initialize()
+
+		//server.
+		//server = ghttp.NewServer()
+		//os.Setenv("http://flask_api_one:5000", server.Addr())
+		//server.AppendHandlers(
+		//	ghttp.CombineHandlers(
+		//		ghttp.VerifyRequest("GET", "/api_info"),
+		//		ghttp.RespondWithJSONEncoded(http.StatusOK, `{"api_name": "flask_api_one", "info": {"framework": "flask", "language": "python"}}`),
+		//	),
+		//)
+		//httpmock.Activate()
 	})
+
+	//AfterEach(func() {
+		//server.Close()
+	//	httpmock.DeactivateAndReset()
+	//})
 
 	Describe("Apis", func() {
 		Context("Request Headerにjwt tokenが存在する場合", func() {
 			Context("Redisに認証情報が存在する場合", func() {
 				// 他APIへRequestできる状態
 				Context("全てのAPIから200が返却された場合", func() {
+					//BeforeEach(func() {
+						//returnedJson = `{"api_name": "flask_api_one", "info": {"framework": "flask", "language": "python"}}`
+						//statusCode = 200
+					//})
+					//httpmock.RegisterResponder("GET", "http://flask_api_one:5000/api_info",
+					//	httpmock.NewStringResponder(200, `{"api_name": "flask_api_one", "info": {"framework": "flask", "language": "python"}}`))
+
 					It("200を返却する", func() {
+						server := httptest.NewServer(http.HandlerFunc(func(rs http.ResponseWriter, rq *http.Request) {
+							jsonStr := `{"api_name": "flask_api_one", "info": {"framework": "flask", "language": "python"}}`
+							//rs.Write()
+							rs.Write(([]byte)(jsonStr))
+							//if rq.RequestURI == `http://flask_api_one:5000/api_info` {}
+						}))
+						defer server.Close()
+
 						req, _ := http.NewRequest("GET", "/v1/apis", nil)
 						response := executeRequest(req)
 
--- bff_api/go.mod
+++ bff_api/go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/golang/protobuf v1.3.2 // indirect
 	github.com/gorilla/mux v1.7.4
+	github.com/jarcoal/httpmock v1.0.4
 	github.com/onsi/ginkgo v1.12.0
 	github.com/onsi/gomega v1.7.1
 )
